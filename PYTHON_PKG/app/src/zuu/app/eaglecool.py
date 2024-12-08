ef0bca8aa8219b56c64e2ac5a38724e96a1f0320
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-12-08 11:25:46 -0800

    feat(py/app/eagle): add methods for library modify

--- PYTHON_PKG/app/src/zuu/app/eaglecool.py
+++ PYTHON_PKG/app/src/zuu/app/eaglecool.py
@@ -1,6 +1,7 @@
 import os
 import time
 import json
+import typing
 
 def new_ver4_library(path: str):
     os.makedirs(path, exist_ok=True)
@@ -35,3 +36,36 @@ def new_ver4_library(path: str):
     if not os.path.exists(os.path.join(path, "metadata.json")):
         with open(os.path.join(path, "metadata.json"), "w") as f:
             json.dump(metadata, f)
+
+# roaming folder
+settings_path = os.path.join(os.getenv("APPDATA"), "Eagle", "Settings")
+
+def update_library_pathes(pathes: typing.Union[dict, typing.List[str]]):
+    if isinstance(pathes, list):
+        with open(settings_path, "r") as f:
+            settings = json.load(f)
+    elif isinstance(pathes, dict):
+        settings = pathes
+    else:
+        raise ValueError("pathes must be a list or a dict")
+
+    settings["libraryHistory"] = pathes
+
+    with open(os.path.join(settings_path, "settings.json"), "w") as f:
+        json.dump(settings, f)
+
+def add_library_path(path: str):
+    with open(settings_path, "r") as f:
+        settings = json.load(f)
+
+    settings["libraryHistory"].append(path)
+
+    update_library_pathes(settings)
+
+def remove_library_path(path: str):
+    with open(settings_path, "r") as f:
+        settings = json.load(f)
+
+    settings["libraryHistory"].remove(path)
+
+    update_library_pathes(settings)
